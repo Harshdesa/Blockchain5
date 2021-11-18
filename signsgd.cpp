41b74e3a077bcca05e1b741dbed258f198695d30
Harshdesa <Harshdesa@users.noreply.github.com>
2021-11-18 15:58:55 -0600

    Update signsgd.cpp

--- signsgd.cpp
+++ signsgd.cpp
@@ -334,7 +334,7 @@ std::string printprivatekey(int mod_size, int exp_size, const unsigned char *p_r
 
 
 type Payload struct {
-	Parameters []float64
+	Parameters []byte
 }
 
 type Metadata struct {
@@ -345,14 +345,20 @@ type Metadata struct {
 // Init is called during chaincode instantiation to initialize any
 // data. Note that chaincode upgrade also calls this function to reset
 // or to migrate data.
-func (t *SimpleAsset) Init(ctx contractapi.TransactionContextInterface, NodeA string, NodeAParameters[] float64) error {
+func (t *SimpleAsset) Init(ctx contractapi.TransactionContextInterface, NodeA string, NodeAParameters string) error {
 
  
+        NodeAData, err := base64.StdEncoding.DecodeString(NodeAParameters)
+        if err != nil {
+		fmt.Println("error:", err)
+		return err
+	}
+
 	fmt.Printf("NodeAParameters = %s", NodeAParameters)
 	// Write the state to the ledger
 
         NodeAPayloadJson := Payload{
-		Parameters: NodeAParameters,
+		Parameters: NodeAData,
 	}
 
         NodeASubparty := []string {NodeA+"0"}
@@ -363,20 +369,93 @@ func (t *SimpleAsset) Init(ctx contractapi.TransactionContextInterface, NodeA st
         NodeAMetadata, err := json.Marshal(NodeAMetadataJson)
 	err = ctx.GetStub().PutState(NodeAMetadataJson.Subparty[0], NodeAPayload)
         err = ctx.GetStub().PutState(NodeA, NodeAMetadata)
+
+	return nil
+
+}
+
+func (t *SimpleAsset) Set(ctx contractapi.TransactionContextInterface, NodeName string, NodeParameters string) error {
+
+
+        // Get the state from the ledger
+        NodeMetadataBytes, err := ctx.GetStub().GetState(NodeName)
         if err != nil {
           return err
         }
+
+        if NodeMetadataBytes == nil {
+
+                NodeData, err := base64.StdEncoding.DecodeString(NodeParameters)
+                if err != nil {
+                  fmt.Println("error:", err)
+                  return err
+                }
+
+                NodePayloadJson := Payload{
+                       Parameters: NodeData,
+                }
+
+                NodeBaseSubparty := []string {NodeName+"0"}
+                NodeMetadataJson := Metadata{
+                       Subparty: NodeBaseSubparty, 
+                }
+                NodePayload, err := json.Marshal(NodePayloadJson)
+                NodeMetadata, err := json.Marshal(NodeMetadataJson)
+
+                err = ctx.GetStub().PutState(NodeMetadataJson.Subparty[0], NodePayload)
+                err = ctx.GetStub().PutState(NodeName, NodeMetadata)
+
+                if err != nil {
+                        return err
+                }
+        } else {
+            CurrentNodeMetadata := new(Metadata)
+            _ = json.Unmarshal(NodeMetadataBytes, CurrentNodeMetadata)
+
+            var Subparties []string
+            Subparties = make([]string, 4687500, 4687500)
+            Subparties = CurrentNodeMetadata.Subparty
+            SubpartyIndex := len(Subparties)
+            CurrentSubparty := []string {NodeName + strconv.Itoa(SubpartyIndex)}
+            Subparties = append(Subparties, CurrentSubparty...)
+
+	    // Write the state to the ledger
+
+            NodeMetadataJson := Metadata{
+	    	Subparty: Subparties,
+	    }
+
+            NodeData, err := base64.StdEncoding.DecodeString(NodeParameters)
+            if err != nil {
+              fmt.Println("error:", err)
+              return err
+            }
+            NodePayloadJson := Payload{
+                       Parameters: NodeData,
+            }
+	    NodeMetadata, err := json.Marshal(NodeMetadataJson)
+            NodePayload, err := json.Marshal(NodePayloadJson)
+
+	    err = ctx.GetStub().PutState(NodeName, NodeMetadata)
+            err = ctx.GetStub().PutState(CurrentSubparty[0], NodePayload)
+	    if err != nil {
+		return err
+	    }
+       }
 
 	return nil
 
 }
 
 
+// Invoke is called per transaction on the chaincode. Each transaction is
+// either a 'get' or a 'set' on the asset created by Init function. The Set
+// method may create a new asset by specifying a new key-value pair.
 func (t *SimpleAsset) Invoke(ctx contractapi.TransactionContextInterface, NodeNames [] string, ResultNodeName string, beg int, end int) error {
 	// Extract the function and args from the transaction proposal
         //var err error
-        var ResultParameters []float64
-        ResultParameters = make([]float64, 4687500, 4687500)
+        var ResultParameters []byte
+        ResultParameters = make([]byte, 4687500, 4687500)
         ResultParameters = nil
         
         allNodesMetadata := []Metadata{}
@@ -386,7 +465,7 @@ func (t *SimpleAsset) Invoke(ctx contractapi.TransactionContextInterface, NodeNa
         //STORE METADATA
         for i := 0; i < len(NodeNames) ; i++ {
           NodeMetadataBytes, err := ctx.GetStub().GetState(NodeNames[i])
-          //fmt.Printf("NodeMetadataBytes: %b\n\n", NodeMetadataBytes )
+          fmt.Printf("NodeMetadataBytes: %b\n\n", NodeMetadataBytes )
           if err != nil {
                 return fmt.Errorf("Failed to get state")
           }
@@ -432,23 +511,35 @@ func (t *SimpleAsset) Invoke(ctx contractapi.TransactionContextInterface, NodeNa
 
             //For each 8 bit Parameter in a Batch
             for i := 0; i < len(batch[0].Parameters) ; i++ {
-              var finalParameter float64 = 0.0
+              var finalMajorityBYTEParameter byte = 0
+
+              //For each Bit of the 8 bit Parameter
+              for x:= 0 ; x < 8 ; x++ {
+                var mask byte = 1<<x     //DO SHIFT LEFT OP HERE
+                //TRACE LOG
+                fmt.Printf("Masker: %b\n", mask)                
+                var aggregateParameter uint64 = 0
 
                 //For each worker node
                 for nodeNumber := 0; nodeNumber < numberOfNodes; nodeNumber++ {
-                  finalParameter = finalParameter + batch[nodeNumber].Parameters[i]
+                  if ((batch[nodeNumber].Parameters[i] & mask) > 0) {
+                    aggregateParameter++
+                  }
                   //TRACE LOG
-                  //fmt.Printf("aggregateParameter after & with node %d : %f\n", nodeNumber, finalParameter)
+                  //fmt.Printf("aggregateParameter after & with node %d : %d\n", nodeNumber, aggregateParameter)
                 }
                 // END of for each worker node || aggregateParameter is addition of all the bits
-                finalParameter = finalParameter / float64(numberOfNodes)
+                if (aggregateParameter > (uint64(numberOfNodes/2)))  {
+                   finalMajorityBYTEParameter = finalMajorityBYTEParameter | mask
                    //TRACE LOG
-                   //fmt.Printf("Final Number : %f\n", finalParameter)
+                   //fmt.Printf("Final Number : %b\n", finalMajorityBYTEParameter)
+                }
+              }
               //END of For each Bit of the 64 bit Parameter || finalMajorityUINT64Parameter is the majority
-              //fmt.Printf("Final Number : %f\n", finalParameter)
-              ResultParameters = append(ResultParameters, finalParameter)
-              //fmt.Printf("length of ResultParameters[%d] : %b",i,ResultParameters[i])
-              //fmt.Printf("Subparty: %d",batchNumber)
+              fmt.Printf("Final Number : %b\n", finalMajorityBYTEParameter)
+              ResultParameters = append(ResultParameters, finalMajorityBYTEParameter)
+              fmt.Printf("length of ResultParameters[%d] : %b",i,ResultParameters[i])
+              fmt.Printf("Subparty: %d",batchNumber)
             }
             //END of For each 64 bit Parameter in a Batch || finalMajorityUINT64Parameter is loaded into ResultParameters
 
@@ -477,71 +568,6 @@ func (t *SimpleAsset) Invoke(ctx contractapi.TransactionContextInterface, NodeNa
         }
         return nil
 }
-
-
-
-
-std::string setParameters(shim_ctx_ptr_t ctx, std::string NodeName , float64 NodeParameters[]) {
-
-
-        // Get the state from the ledger
-        NodeMetadataBytes, err = ctx.GetStub().GetState(NodeName)
-        if err != nil {
-          return err
-        }
-
-        if NodeMetadataBytes == nil {
-
-                NodePayloadJson = Payload{
-                       Parameters: NodeParameters,
-                }
-
-                NodeBaseSubparty = []string {NodeName+"0"}
-                NodeMetadataJson = Metadata{
-                       Subparty: NodeBaseSubparty, 
-                }
-                NodePayload, err = json.Marshal(NodePayloadJson)
-                NodeMetadata, err = json.Marshal(NodeMetadataJson)
-
-                err = ctx.GetStub().PutState(NodeMetadataJson.Subparty[0], NodePayload)
-                err = ctx.GetStub().PutState(NodeName, NodeMetadata)
-
-                if err != nil {
-                        return err
-                }
-        } else {
-            CurrentNodeMetadata = new(Metadata)
-            _ = json.Unmarshal(NodeMetadataBytes, CurrentNodeMetadata)
-
-            var Subparties []string
-            Subparties = make([]string, 4687500, 4687500)
-            Subparties = CurrentNodeMetadata.Subparty
-            SubpartyIndex = len(Subparties)
-            CurrentSubparty = []string {NodeName + strconv.Itoa(SubpartyIndex)}
-            Subparties = append(Subparties, CurrentSubparty...)
-
-	    // Write the state to the ledger
-
-            NodeMetadataJson = Metadata{
-	    	Subparty: Subparties,
-	    }
-
-            NodePayloadJson = Payload{
-                       Parameters: NodeParameters,
-            }
-	    NodeMetadata, err = json.Marshal(NodeMetadataJson)
-            NodePayload, err = json.Marshal(NodePayloadJson)
-
-	    err = ctx.GetStub().PutState(NodeName, NodeMetadata)
-            err = ctx.GetStub().PutState(CurrentSubparty[0], NodePayload)
-	    if err != nil {
-		return err
-	    }
-       }
-	return nil
-}
-
-
 
 
 void *private_key = NULL;
